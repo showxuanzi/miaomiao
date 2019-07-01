d7ed6511eafc20289d001d93e5f576b441bec99e
xmx <oncwnuOHdRkkpMOnhSph6FiU_qys@git.weixin.qq.com>
2019-07-01 15:23:33 +0800

    基本样式

---
+++ src/components/TopHead/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <header id="topHead"> 
+        <h1>{{title}}</h1>
+    </header>
+</template>
+<script>
+    export default {
+        name:'TopHead',
+        props: {
+            title: {
+                type: String,
+                default: "喵喵电影"
+            }
+        }
+    }
+</script>
+<style scoped>
+    #topHead{
+        width: 100%;
+        height: 50px;
+        color: #fff;
+        background: #e54847;
+        border-bottom: 1px solid #e54847;
+        position: fixed;
+        left: 0;
+        top:0;
+    }
+    #topHead h1{
+        font-size: 18px;
+        text-align: center;
+        line-height: 50px;
+        font-weight: normal;
+    }
+    #topHead i{
+        position: absolute;
+        left: 5px;
+        top: 50%;
+        margin-top: -13px;
+        font-size: 26px;
+    }
+</style>
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="main">
+        <TopHead title="我的喵喵"/>
+            <div id="content">
+                <Login></Login>
+            </div>
+        <TabBar/>
+    </div>
+</template>
+<script>
+    import TopHead from '@/components/TopHead';
+    import TabBar from '@/components/TabBar';
+    import Login from '@/components/Login';
+    export default {
+        name: "Mine",
+        components: {
+            TopHead,
+            TabBar,
+            Login
+        }
+    }
+</script>
